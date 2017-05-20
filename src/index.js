a54ff3e0bfb90ec8632c90400f59246131a25dff
Kevin Decock <kevin.decock@district01.be>
2017-05-20 13:39:52 +0200

    updates to package json

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 export { withPadding } from './withPadding';
 export { withMargin } from './withMargin';
 export { createStyleHoc } from './createStyleHoc.js';
-
-export { compose, curry } from 'ramda';
+export { compose } from 'ramda';
