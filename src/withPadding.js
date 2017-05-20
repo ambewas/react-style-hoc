e4d1b719237906f7f3a57ca3db55ae848bcbbbb2
Kevin Decock <kevin.decock@district01.be>
2017-05-20 12:35:30 +0200

    hoc creater works with arrays and style objects as well

--- src/withPadding.js
+++ src/withPadding.js
@@ -1,5 +1,5 @@
 import { curry } from 'ramda';
 import { createStyleHoc } from './createStyleHoc.js';
-// export const withPadding = createStyleHoc({ margin: 10, padding: '10vh' });
+export const withPadding = createStyleHoc({ margin: 10, padding: '10vh' });
 // export const withPadding = createStyleHoc(['margin', 'padding']);
-export const withPadding = createStyleHoc('padding');
+// export const withPadding = createStyleHoc('padding');
